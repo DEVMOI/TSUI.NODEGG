4fe70333d11241d93199066ad4f915af6be5e7be
Warren Gates <warrenckhan@gmail.com>
2019-02-19 17:59:58 -0500

    add web library to the lib dir

--- src/lib/kumo/index.js
+++ src/lib/kumo/index.js
@@ -1,10 +1,1 @@
 console.log('rip')
-// const Canvas = require('./canvas');
-// let canvas = new Canvas();
-// canvas();
-// import log from '../ankh';
-// console.log('hello')
-// log('hello');
-// // initCanvas('app','Kumo','2d','inherit','inherit');
-// const Scene = new Canvas('app', 'Kumo', '2d', 'inherit', 'inherit');
-// console.log(Scene.options)
